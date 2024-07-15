3815fdcad50b677bbaddb2106d6fe882d08709cd
Christoph Kuri <chkuri@chkuri.de>
2024-07-15 13:20:45 +0200

    up one

--- frontend/src/ArticleList.jsx
+++ frontend/src/ArticleList.jsx
@@ -1,3 +1,47 @@
-export default function ArticleList() {
-  return <div>ArticleList</div>;
-}
+import React, { useState, useEffect } from 'react';
+
+const articleTags = (props) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://content.guardianapis.com/tags?q=${props}&section=${props}&api-key=test`
+        );
+        if (!response.ok) {
+          throw new Error('Netzwerkantwort war nicht ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Laden...</div>;
+  }
+
+  if (error) {
+    return <div>Fehler: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>{props} Tags</h1>
+      <ul>
+        {data.response.results.map((tag) => (
+          <li key={tag.id}>{tag.webTitle}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
@@ -8,12 +8,14 @@ const Navbar = () => {
         <div className="flex space-x-4">
           <li className="text-lg cursor-pointer hover:text-blue-500">Sports</li>
           <li className="text-lg cursor-pointer hover:text-blue-500">Music</li>
-          <li className="text-lg cursor-pointer hover:text-blue-500">e-Games</li>
+          <li className="text-lg cursor-pointer hover:text-blue-500">
+            e-Games
+          </li>
           <li className="text-lg cursor-pointer hover:text-blue-500">Travel</li>
         </div>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
